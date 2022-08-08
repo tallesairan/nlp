3e6eb789b54a143593554453b5c7531ca48f4c89
Talles Airan <airan.talles@gmail.com>
2022-08-08 11:19:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def translation(source, target, text):
     model = model_dict[model_name + '_model']
     tokenizer = model_dict[model_name + '_tokenizer']
 
-    translator = pipeline('translation', model=model, num_workers=54, tokenizer=tokenizer, src_lang=source, tgt_lang=target).to('cuda')
-    output = translator(text, max_length=512).to('cuda')
+    translator = pipeline('translation', model=model, num_workers=54, tokenizer=tokenizer, src_lang=source, tgt_lang=target, device=1)
+    output = translator(text, max_length=512, device=1)
 
     end_time = time.time()
 
