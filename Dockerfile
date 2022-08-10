ec5e77ffa181e7c08e15096c4104d457f8ad953d
Talles Airan <airan.talles@gmail.com>
2022-08-10 00:16:17 -0300

    CUDA_VISIBLE_DEVICES
    CUDA_VISIBLE_DEVICES=0,1 + DockerFile

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,15 @@
 FROM nvidia/cuda:11.6.2-devel-ubuntu20.04
 ARG DEBIAN_FRONTEND=noninteractive
-
 RUN apt-get update
-RUN apt-get install -y sudo
-
-RUN useradd -ms /bin/bash nllb
-RUN passwd -d nllb
-RUN usermod -aG sudo nllb
-
-COPY bin/init_setup /home/nllb/
-RUN mkdir /home/nllb/utils
-RUN chown nllb:nllb /home/nllb/init_setup
-RUN chmod 774 /home/nllb/init_setup
-
+RUN apt-get -y upgrade
+RUN apt-get install -y sudo zip git wget curl nano screen htop tzdata build-essential
+RUN useradd -ms /bin/bash nlp
+RUN passwd -d nlp
+RUN usermod -aG sudo nlp
+COPY bin/init_setup /home/nlp/
+RUN mkdir /home/nlp/utils
+RUN chown nlp:nlp /home/nlp/init_setup
+RUN chmod 774 /home/nlp/init_setup
 # Disable tmux for vast.ai
 RUN touch /root/.no_auto_tmux
-
- 
+ENV CUDA_VISIBLE_DEVICES=0,1
---
+++ load-models.py
@@ -0,0 +1,38 @@
+
+from asyncio import threads
+import os
+import torch
+import time
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from fastapi import FastAPI
+
+ 
+def load_models():
+    # build model and tokenizer
+    model_name_dict = {
+                    'gpt-j-6B': 'EleutherAI/gpt-j-6B',
+                    'distilgpt2': 'distilgpt2',
+                    'nllb-distilled': 'facebook/nllb-200-distilled-1.3B',
+                    'opus-mt-mul-en': 'Helsinki-NLP/opus-mt-mul-en',
+                    'bertin-gpt-j-6B':'bertin-project/bertin-gpt-j-6B'
+                }
+
+    model_dict = {}
+
+    for call_name, real_name in model_name_dict.items():
+        print('\tLoading model: %s' % call_name)
+        
+        ## GPTJ WITH CUDA
+        #model = AutoModelForCausalLM.from_pretrained(real_name,  device=0, low_cpu_mem_usage=True) 
+        # GPTJ CPU ONLY
+        model = AutoModelForCausalLM.from_pretrained(real_name)
+        tokenizer = AutoTokenizer.from_pretrained(real_name)
+        model_dict[call_name+'_model'] = model
+        model_dict[call_name+'_tokenizer'] = tokenizer
+        print('\tLoaded model: %s' % call_name)
+    return model_dict
+
+ 
+
+global model_dict
+model_dict = load_models()
