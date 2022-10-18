2bd6f246d07750075905a1dfba133331c4e450dc
Talles Airan <airan.talles@gmail.com>
2022-10-18 19:42:54 -0300

    Create web-api.py

---
+++ InvokeAI/scripts/web-api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Lincoln D. Stein (https://github.com/lstein)
+
+import os
+import re
+import sys
+import shlex
+import copy
+import warnings
+import time
+import traceback
+sys.path.append('.')    # corrects a weird problem on Macs
+f
+
+
+from ldm.generate import Generate
+g       = Generate()
+outputs = g.txt2img("a unicorn in manhattan")
